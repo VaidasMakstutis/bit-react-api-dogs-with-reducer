625c09efb975a8d4ab8ab91f46d6615094985e60
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-01-09 19:33:56 +0200

    Context

---
+++ src/Context/DogsContext.js
@@ -0,0 +1,34 @@
+import { createContext, useReducer, useContext } from "react";
+import DogsReducer from "../Reducers/DogsReducer";
+import { getDogs } from "../Actions/DogsActions";
+import axios from "axios";
+
+const initialState = {
+  dogs: []
+};
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(DogsReducer, initialState);
+
+  const getRandomDog = () => {
+    let dog;
+    try {
+      axios.get(`https://dog.ceo/api/breeds/images/random`).then(res => {
+        dog = res.data.message;
+        dispatch(getDogs(dog));
+        console(dog);
+      });
+    } catch (error) {
+      console.log("Šuns veislė nerasta...");
+    }
+    return <AppContext.Provider value={{ ...state, getRandomDog }}>{children}</AppContext.Provider>;
+  };
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
